ea188636c543b0c46c1c553e293208e02ca3a5f4
LEMONY <jin.lee1028@gmail.com>
2025-06-05 00:17:16 +0900

    [Setting] BottomTabBar 추가

---
+++ SherlDog/BottomTabBarController.swift
@@ -0,0 +1,56 @@
+//
+//  BottomTabBarController.swift
+//  SherlDog
+//
+//  Created by JIN LEE on 6/5/25.
+//
+
+import UIKit
+
+class BottomTabBarController: UITabBarController {
+    /*
+    let VC1 = ViewController()
+    let VC2 = ViewController()
+    let VC3 = ViewController()
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let controllers = [VC1, VC2, VC3]
+        self.viewControllers = controllers
+        
+        setupTabBar()
+        configureTabBar()
+    }
+    
+    private func setupTabBar() {
+        
+     VC1.tabBarItem = UITabBarItem(title: "알람", image: UIImage(systemName: "alarm"), tag: 1)
+     VC1.tabBarItem.selectedImage = UIImage(systemName: "alarm.fill")
+        
+     VC2.tabBarItem = UITabBarItem(title: "타이머", image: UIImage(systemName: "timer"), tag: 0)
+     VC2.tabBarItem.selectedImage = UIImage(systemName: "gauge.with.needle.fill")
+        
+     VC3.tabBarItem = UITabBarItem(title: "스톱워치", image: UIImage(systemName: "stopwatch"), tag: 2)
+     VC3.tabBarItem.selectedImage = UIImage(systemName: "stopwatch.fill")
+    }
+    
+    private func configureTabBar() {
+        
+        let attributes = [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 13, weight: .bold)]
+        
+        let appearance = UITabBarAppearance()
+        
+        appearance.backgroundEffect = UIBlurEffect(style: .light)
+        //standardAppearance랑 scrollEdgeAppearance 둘 다 지정해줘야 UITabBarAppearance()에 지정한 스타일이 먹음.
+        UITabBar.appearance().standardAppearance = appearance
+        UITabBar.appearance().scrollEdgeAppearance = appearance
+        UITabBar.appearance().unselectedItemTintColor = .white
+        UITabBar.appearance().tintColor = .white
+        UITabBar.appearance().selectedItem?.setTitleTextAttributes(attributes, for: .normal)
+        UITabBar.appearance().selectedItem?.setTitleTextAttributes(attributes, for: .selected)
+    }
+     */
+}
+
