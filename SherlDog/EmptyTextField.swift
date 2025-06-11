bb06b092c28ed775631cb29576255022a7c08c21
LEMONY <jin.lee1028@gmail.com>
2025-06-11 12:14:17 +0900

    [Design] #1 - 멍탐정 등록 프로필 뼈대 구현

---
+++ SherlDog/EmptyTextField.swift
@@ -0,0 +1,34 @@
+//
+//  EmptyTextField.swift
+//  SherlDog
+//
+//  Created by Jin Lee on 6/10/25.
+//
+//
+//import UIKit
+//import SnapKit
+//
+//class EmptyTextField: UITextField {
+//        
+//    override init(frame: CGRect) {
+//        super.init(frame: frame)
+//        setupUI()
+//        configureUI()
+//    }
+//    
+//    required init?(coder: NSCoder) {
+//        fatalError("init(coder:) has not been implemented")
+//    }
+//    
+//    private func setupUI() {
+//    }
+//    
+//    private func configureUI() {
+//        background.snp.makeConstraints {
+//            $0.top.equalToSuperview()
+//            $0.leading.trailing.equalToSuperview()
+//            
+//        }
+//    }
+//    
+//}
