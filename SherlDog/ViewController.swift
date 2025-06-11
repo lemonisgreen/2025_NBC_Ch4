9e7fe0e711057d77aeee555603144af7110dcb32
ghnn-n <cgh6470@gmail.com>
2025-06-11 17:59:28 +0900

    [Chore] #31 - 테스트코드 삭제

--- SherlDog/ViewController.swift
+++ SherlDog/ViewController.swift
@@ -10,18 +10,5 @@ import UIKit
 class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let vc = UINavigationController(rootViewController: PictureUploadRequestView(viewModel: PictureUploadRequestViewModel()))
-        
-        vc.modalPresentationStyle = .pageSheet
-        
-        if let sheet = vc.sheetPresentationController {
-            sheet.detents = [.medium()]
-            sheet.selectedDetentIdentifier = .medium
-            sheet.prefersGrabberVisible = true
-            sheet.preferredCornerRadius = 32
-        }
-        
-        self.present(vc, animated: true)
     }
 }
