04c40cc4ec6dd03bf21387d12bb3cedd1c738e4b
ghnn-n <cgh6470@gmail.com>
2025-06-12 15:36:33 +0900

    [Design] #47 - 커뮤니티 뷰 UI 구현

---
+++ SherlDog/Scene/CommunityTap/CommunityCellDelegate.swift
@@ -0,0 +1,11 @@
+//
+//  CommunityCellDelegate.swift
+//  SherlDog
+//
+//  Created by 최규현 on 6/12/25.
+//
+
+// 추후 작업 예정
+//protocol CommunityCellDelegate: AnyObject {
+//    func didTapMoreShowButton(in cell: CommunityCell)
+//}
---
+++ SherlDog/Scene/CommunityTap/CommunityModel.swift
@@ -0,0 +1,46 @@
+//
+//  CommunityModelTest.swift
+//  SherlDog
+//
+//  Created by 최규현 on 6/12/25.
+//
+
+import UIKit
+
+// MARK: - CommunityModel
+struct CommunityModel {
+    let profileImage: UIImage?
+    let name: String
+    let info: String
+    let postDate: String
+    let contentImage: UIImage?
+    let content: String
+    var isExpanded: Bool
+}
+
+// test
+extension CommunityModel {
+    static let sample: [CommunityModel] = [
+        CommunityModel(profileImage: UIImage(named: "bigLogo"),
+                       name: "강아지명",
+                       info: "3세 여아 / 말티즈",
+                       postDate: "1시간",
+                       contentImage: .kakao,
+                       content: "testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest",
+                       isExpanded: false),
+        CommunityModel(profileImage: UIImage(named: "apple"),
+                       name: "강아지명",
+                       info: "3세 여아 / 말티즈",
+                       postDate: "1시간",
+                       contentImage: .naver,
+                       content: "testtesttesttestteststtesttesttest",
+                       isExpanded: false),
+        CommunityModel(profileImage: UIImage(named: "avatar"),
+                       name: "강아지명",
+                       info: "3세 여아 / 말티즈",
+                       postDate: "1시간",
+                       contentImage: .logo,
+                       content: "testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest",
+                       isExpanded: false)
+    ]
+}
