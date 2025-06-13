beaf25348cf6484de0ebaed82df3a8300af54aad
Wonsik1 <jos505000@gmail.com>
2025-06-13 13:51:56 +0900

    [Fix] 패키지 문제로 인한 재 커밋

--- SherlDog/Reusable/Component/BottomTabBarController.swift
+++ SherlDog/Reusable/Component/BottomTabBarController.swift
@@ -8,11 +8,11 @@
 import UIKit
 
 class BottomTabBarController: UITabBarController {
-    /*
-    let VC1 = ViewController()
+    
+    let VC1 = WalkMainViewController()
     let VC2 = ViewController()
     let VC3 = ViewController()
-
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,31 +26,48 @@ class BottomTabBarController: UITabBarController {
     
     private func setupTabBar() {
         
-     VC1.tabBarItem = UITabBarItem(title: "알람", image: UIImage(systemName: "alarm"), tag: 1)
-     VC1.tabBarItem.selectedImage = UIImage(systemName: "alarm.fill")
+        VC1.tabBarItem = UITabBarItem(title: "홈", image: UIImage(named: "home")?.resized(to: CGSize(width: 32, height: 32)),tag: 1)
+        VC1.tabBarItem.selectedImage = UIImage(named: "homeGreen")?.resized(to: CGSize(width: 32, height: 32)).withRenderingMode(.alwaysOriginal)
         
-     VC2.tabBarItem = UITabBarItem(title: "타이머", image: UIImage(systemName: "timer"), tag: 0)
-     VC2.tabBarItem.selectedImage = UIImage(systemName: "gauge.with.needle.fill")
+        VC2.tabBarItem = UITabBarItem(title: "커뮤니티", image: UIImage(named: "community")?.resized(to: CGSize(width: 32, height: 32)),tag: 0)
+        VC2.tabBarItem.selectedImage = UIImage(named: "communityGreen")?.resized(to: CGSize(width: 32, height: 32)).withRenderingMode(.alwaysOriginal)
         
-     VC3.tabBarItem = UITabBarItem(title: "스톱워치", image: UIImage(systemName: "stopwatch"), tag: 2)
-     VC3.tabBarItem.selectedImage = UIImage(systemName: "stopwatch.fill")
+        VC3.tabBarItem = UITabBarItem(title: "마이", image: UIImage(named: "myPage")?.resized(to: CGSize(width: 32, height: 32)),tag: 2)
+        VC3.tabBarItem.selectedImage = UIImage(named: "myPageGreen")?.resized(to: CGSize(width: 32, height: 32)).withRenderingMode(.alwaysOriginal)
     }
     
     private func configureTabBar() {
         
-        let attributes = [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 13, weight: .bold)]
+        let selectedAttributes: [NSAttributedString.Key: Any] = [
+            .font: UIFont.body5,
+            .foregroundColor: UIColor.textPrimary
+        ]
+
+        let unselectedAttributes: [NSAttributedString.Key: Any] = [
+            .font: UIFont.body5,
+            .foregroundColor: UIColor.gray300
+        ]
         
         let appearance = UITabBarAppearance()
         
         appearance.backgroundEffect = UIBlurEffect(style: .light)
         //standardAppearance랑 scrollEdgeAppearance 둘 다 지정해줘야 UITabBarAppearance()에 지정한 스타일이 먹음.
+        appearance.stackedLayoutAppearance.selected.titleTextAttributes = selectedAttributes
+        appearance.stackedLayoutAppearance.normal.titleTextAttributes = unselectedAttributes
+        
         UITabBar.appearance().standardAppearance = appearance
         UITabBar.appearance().scrollEdgeAppearance = appearance
         UITabBar.appearance().unselectedItemTintColor = .white
         UITabBar.appearance().tintColor = .white
-        UITabBar.appearance().selectedItem?.setTitleTextAttributes(attributes, for: .normal)
-        UITabBar.appearance().selectedItem?.setTitleTextAttributes(attributes, for: .selected)
     }
-     */
+    
 }
 
+extension UIImage {
+    func resized(to size: CGSize) -> UIImage {
+        let renderer = UIGraphicsImageRenderer(size: size)
+        return renderer.image { _ in
+            self.draw(in: CGRect(origin: .zero, size: size))
+        }
+    }
+}
