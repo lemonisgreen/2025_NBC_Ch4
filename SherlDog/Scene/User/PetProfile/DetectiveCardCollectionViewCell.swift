f50ac86639a51d36f11893c02cc35216cfe005ce
yeungrak <tifshadk@gmail.com>
2025-06-16 20:22:12 +0900

    [중간저장]

---
+++ SherlDog/Scene/User/PetProfile/DetectiveCardCollectionViewCell.swift
@@ -0,0 +1,41 @@
+//
+//  Untitled.swift
+//  SherlDog
+//
+//  Created by 최영락 on 6/16/25.
+//
+
+class DetectiveCardCollectionViewCell: UICollectionViewCell {
+    
+    static let identifier = "DetectiveCardCollectionViewCell"
+    
+    private let detectiveCardView = DetectiveCardView()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupUI() {
+        contentView.addSubview(detectiveCardView)
+        detectiveCardView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+    
+    func configure(with profile: PetProfile) {
+        detectiveCardView.detectiveNumber.text = profile.uid
+        detectiveCardView.detectiveName.text = profile.name
+        detectiveCardView.detectiveBreed.text = profile.breed
+        detectiveCardView.detectiveAge.text = "\(profile.age)세"
+        detectiveCardView.detectiveIntroduce.text = "# \(profile.introduce)"
+        
+        // 프로필 이미지 설정
+        detectiveCardView.detectivePhotoImageView.backgroundColor = .gray400
+        // TODO: 실제 이미지 로딩 시 profile.image 사용
+    }
+}
