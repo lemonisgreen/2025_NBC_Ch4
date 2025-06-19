59eaf7eaf231b63e79eef7f56e5050175906d907
LEMONY <jin.lee1028@gmail.com>
2025-06-19 18:00:32 +0900

    [Feat] #121 -  텍스트뷰에서 키보드 엔터 -> 완료로 바꾸고 키보드 내리기

--- SherlDog/Scene/User/HumanProfile/CreateAssistantProfileViewController.swift
+++ SherlDog/Scene/User/HumanProfile/CreateAssistantProfileViewController.swift
@@ -228,6 +228,9 @@ extension CreateAssistantProfileViewController {
         introduceTextView.backgroundColor = .gray50
         introduceTextView.layer.cornerRadius = 6
         introduceTextView.textContainerInset = .init(top: 12, left: 8, bottom: 12, right: 8)
+        // 키보드 완료 버튼으로 만들기
+        introduceTextView.delegate = self
+        introduceTextView.returnKeyType = .done
         
         introduceConstraintsLabel.text = "0 / 150자"
         introduceConstraintsLabel.font = .alert2
@@ -317,4 +320,14 @@ extension CreateAssistantProfileViewController {
         }
     }
     
+}
+// 키보드 완료 버튼 익스텐션
+extension CreateAssistantProfileViewController: UITextViewDelegate {
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        if text == "\n" {
+            textView.resignFirstResponder() // 키보드 내림
+            return false // 개행문자 입력 방지
+        }
+        return true
+    }
 }
