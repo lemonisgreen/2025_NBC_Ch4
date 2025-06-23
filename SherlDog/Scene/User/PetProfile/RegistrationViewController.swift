724fc431e03ffaf0f24e82d391d070fb8f0671f4
ghnn-n <cgh6470@gmail.com>
2025-06-23 17:08:26 +0900

    [Fix] #148 - 다음 버튼 활성화 조건에 중성화 여부 추가, 해당 로직 코드 리팩토링

--- SherlDog/Scene/User/PetProfile/RegistrationViewController.swift
+++ SherlDog/Scene/User/PetProfile/RegistrationViewController.swift
@@ -78,30 +78,24 @@ class RegistrationViewController: UIViewController {
     
     func bind() {
         Observable.combineLatest(
-            self.cameraViewModel.output.capturedImage.map {
-                guard $0 != nil else { return "" }
-                return "set"
-            },
+            self.cameraViewModel.output.capturedImage,
             self.viewModel.name,
             self.viewModel.breed,
             self.viewModel.selectedSize,
             self.viewModel.selectedGender,
+            self.viewModel.isNeutered,
             self.viewModel.introduce,
-            self.viewModel.selectedAge.map {
-                guard let date = $0 else { return "" }
-                let formatter = DateFormatter()
-                formatter.dateFormat = "yyyy"
-                return formatter.string(from: date)
-            }
+            self.viewModel.selectedAge
         )
-        .subscribe(onNext: { [weak self] image, name, breed, size, gender, introduce, age in
-            if image.count > 0,
+        .subscribe(onNext: { [weak self] image, name, breed, size, gender, isNeutered, introduce, age in
+            if image != nil,
                name.count > 0,
                breed.count > 0,
                size.count > 0,
                gender.count > 0,
+               isNeutered != nil,
                introduce.count > 0,
-               age.count > 0 {
+               age != nil {
                 self?.registCompletButton.isEnabled = true
             } else {
                 self?.registCompletButton.isEnabled = false
