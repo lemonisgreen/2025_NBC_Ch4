7c5d0d9dea526f0a9dcc5f81c5d44a3692d77417
ghnn-n <105583518+ghnn-n@users.noreply.github.com>
2025-06-25 16:11:07 +0900

    [Fix] #174 - 루트 뷰 원복

--- SherlDog/Application/SceneDelegate.swift
+++ SherlDog/Application/SceneDelegate.swift
@@ -21,7 +21,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // 로그인 상태 확인 후 초기 화면 결정
        let initialViewController = determineInitialViewController()
         
-        window.rootViewController = UINavigationController(rootViewController: InvLogListViewController())
+        window.rootViewController = initialViewController
         self.window = window
         window.makeKeyAndVisible()
     }
