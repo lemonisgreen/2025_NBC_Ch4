2abb349b2b215190cf5f7517cbb1751c168757d8
LEMONY <jin.lee1028@gmail.com>
2025-06-26 15:51:28 +0900

    [Feat] #189 - 마이페이지 멍탐정 카드에 강아지 프로필 데이터 연결하기

--- SherlDog/Scene/MyPageTap/DetectiveCardCell.swift
+++ SherlDog/Scene/MyPageTap/DetectiveCardCell.swift
@@ -13,13 +13,42 @@ class DetectiveCardCell: UICollectionViewCell {
     
     override init(frame: CGRect) {
         super.init(frame: frame)
+        setupUI()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupUI() {
         contentView.addSubview(cardView)
         cardView.snp.makeConstraints {
             $0.edges.equalToSuperview()
         }
     }
-
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+    
+    func configure(with profile: PetProfile) {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd"
+        var age: Int = 0
+        var detectiveNumber: String = "?"
+        
+        if let birthDate = formatter.date(from: profile.age) {
+            age = Calendar.current.dateComponents([.year], from: birthDate, to: Date()).year ?? 0
+            let detectiveNumberFormatter = DateFormatter()
+            detectiveNumberFormatter.dateFormat = "yyMMdd"
+            detectiveNumber = detectiveNumberFormatter.string(from: birthDate)
+        }
+        
+        cardView.detectiveNumber.text = detectiveNumber
+        cardView.detectiveName.text = profile.name
+        cardView.detectiveBreed.text = profile.breed
+        cardView.detectiveAge.text = "\(age)세"
+        cardView.detectiveIntroduce.text = "# \(profile.introduce)"
+        FirebaseImageManager.shared.downloadPetImage(petId: profile.petProfileId, userId: profile.userId) { [weak self] image in
+            DispatchQueue.main.async {
+                self?.cardView.detectivePhotoImageView.image = image
+            }
+        }
     }
 }
