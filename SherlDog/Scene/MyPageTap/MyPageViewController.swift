99bd74ff0a845c10c9cf2468c1606ce8ff8a332a
Wonsik1 <jos505000@gmail.com>
2025-06-27 15:46:11 +0900

    [Fix] 인덱스 닷 컬러 수정

--- SherlDog/Scene/MyPageTap/MyPageViewController.swift
+++ SherlDog/Scene/MyPageTap/MyPageViewController.swift
@@ -109,8 +109,8 @@ class MyPageViewController : UIViewController {
         
         pageControl.numberOfPages = 0
         pageControl.currentPage = 0
-        pageControl.pageIndicatorTintColor = .gray400
-        pageControl.currentPageIndicatorTintColor = .gray500
+        pageControl.pageIndicatorTintColor = .keycolorPrimary2
+        pageControl.currentPageIndicatorTintColor = .keycolorPrimary5
         
         buttonStack.axis = .vertical
         buttonStack.distribution = .fillEqually
