f1fd64c4dcb5560926bc27da3328e1131313b3d6
LEMONY <jin.lee1028@gmail.com>
2025-07-02 17:51:16 +0900

    [Fix] #255 - 멍탐정 카드셀 백그라운드 clear로 변경 (하얀 모서리 제거)

--- SherlDog/Scene/User/PetProfile/DetectiveCardCollectionViewCell.swift
+++ SherlDog/Scene/User/PetProfile/DetectiveCardCollectionViewCell.swift
@@ -44,7 +44,7 @@ class DetectiveCardCollectionViewCell: UICollectionViewCell {
             detectiveNumber = detectiveNumberFormatter.string(from: birthDate)
         }
         
-        self.backgroundColor = .keycolorInverse
+        self.backgroundColor = .clear
         detectiveCardView.detectiveNumber.text = detectiveNumber
         detectiveCardView.detectiveName.text = profile.name
         detectiveCardView.detectiveBreed.text = profile.breed
