a8f27198fa3be2f9b20e9c703e2f4e8008948bec
LEMONY <jin.lee1028@gmail.com>
2025-08-18 17:49:56 +0900

    [Fix] #324 - 아이폰 미니/SE에서 걸음 수 패딩값이 안 맞아보이는 문제 해결

--- SherlDog/Scene/HomeTab/Investigation/View/WalkEndModalViewController.swift
+++ SherlDog/Scene/HomeTab/Investigation/View/WalkEndModalViewController.swift
@@ -345,7 +345,7 @@ class WalkEndModalViewController : UIViewController {
         
         todayLabel.text = "2025/06/05"
         todayLabel.textColor = UIColor(named: "keycolorPrimary2")
-        todayLabel.font = (UIScreen.isIPhoneSE || UIScreen.isIPhoneMini) ? .recordTitleIsSE : .recordTitle 
+        todayLabel.font = (UIScreen.isIPhoneSE || UIScreen.isIPhoneMini) ? .recordTitleIsSE : .recordTitle
         todayLabel.backgroundColor = .clear
         
         distanceLabel.text = "거리"
@@ -529,8 +529,14 @@ class WalkEndModalViewController : UIViewController {
             $0.leading.trailing.equalToSuperview().inset(30)
         }
         
-        stepCountLabel.snp.makeConstraints {
-            $0.trailing.equalToSuperview().inset(55)
+        if (UIScreen.isIPhoneSE || UIScreen.isIPhoneMini) {
+            stepCountLabel.snp.makeConstraints {
+                $0.trailing.equalToSuperview().inset(45)
+            }
+        } else {
+            stepCountLabel.snp.makeConstraints {
+                $0.trailing.equalToSuperview().inset(55)
+            }
         }
         
         stepCountContentLabel.snp.makeConstraints {
