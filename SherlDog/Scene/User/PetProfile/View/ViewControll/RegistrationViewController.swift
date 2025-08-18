1fe438d5d8d693e4bf2be549d8c77fed65800140
LEMONY <jin.lee1028@gmail.com>
2025-08-18 20:19:26 +0900

    [Fix] #323 - 기능 선택 모달창 선택 완료 버튼 없이 해당 기능페이지로 넘어가게 수정
    복수선택 모달은 제외

--- SherlDog/Scene/User/PetProfile/View/ViewControll/RegistrationViewController.swift
+++ SherlDog/Scene/User/PetProfile/View/ViewControll/RegistrationViewController.swift
@@ -262,7 +262,7 @@ class RegistrationViewController: UIViewController {
                 requestView.modalPresentationStyle = .pageSheet
                 
                 if let sheet = requestView.sheetPresentationController {
-                    sheet.detents = [.custom { _ in 400 }]
+                    sheet.detents = [.custom { _ in 340 }]
                     sheet.selectedDetentIdentifier = .medium
                     sheet.prefersGrabberVisible = true
                     sheet.preferredCornerRadius = 20
