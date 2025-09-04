eee28a976ef44a335dcca1f273beb2f4a0fb3124
LEMONY <jin.lee1028@gmail.com>
2025-09-04 12:50:32 +0900

    [Feat] #318 - 메인탭바 기존 코드로 복구

--- SherlDog/Reusable/Component/BottomTabBarController.swift
+++ SherlDog/Reusable/Component/BottomTabBarController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class BottomTabBarController: UITabBarController {
     
-    let mainVC = UINavigationController(rootViewController: UserProfileViewController())
+    let mainVC = UINavigationController(rootViewController: MainViewController())
     let communityVC = UINavigationController(rootViewController: CommunityViewController())
     let myPageVC = UINavigationController(rootViewController: MyPageViewController())
 
