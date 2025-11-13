021ea11a1f88b4f7f7bbb90ae790245eca8dcd58
LEMONY <jin.lee1028@gmail.com>
2025-11-13 15:31:11 +0900

    [Fix] 수사 게시판 게시글만 하단정렬로 이미지 보이게 수정

--- SherlDog/Scene/CommunityTab/View/CommunityViewController.swift
+++ SherlDog/Scene/CommunityTab/View/CommunityViewController.swift
@@ -264,8 +264,13 @@ extension CommunityViewController {
                 guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MediaCell.identifier, for: indexPath) as? MediaCell else { return .init() }
                 let petProfile = dataSource.sectionModels[indexPath.section].model.petProfile
                 
-                cell.settingCell(item)
-                cell.settingPetProfile(profile: petProfile)
+                let category = CommunitySectionType.allCases[self.segmentedControl.selectedSegmentIndex]
+
+                cell.configureForCommunityPost(
+                    imageURL: item,
+                    petProfiles: petProfile,
+                    category: category
+                )
                 
                 cell.rx.mediaDoubleTap
                     .map { _ in dataSource.sectionModels[indexPath.section].model }
