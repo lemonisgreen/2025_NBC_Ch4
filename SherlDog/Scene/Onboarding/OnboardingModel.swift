3718f27f7fab6f6cd311ab1fa9d9af8587fd5bad
LEMONY <jin.lee1028@gmail.com>
2026-03-10 21:03:03 +0900

    [Fix] #438 - 온보딩 문구 수정

--- SherlDog/Scene/Onboarding/OnboardingModel.swift
+++ SherlDog/Scene/Onboarding/OnboardingModel.swift
@@ -16,7 +16,7 @@ struct OnboardingModel {
             text: "반려견은 멍탐정,\n당신은 멍탐정의 조수!\n매일의 산책,\n오늘은 수사로 떠나봐요.",
             lottieName: "Onboarding1"),
         OnboardingStep(
-            text: "수사 중 만난 풍경이나 순간을\n단서로 남기고, 다른 멍탐정들이\n남긴 단서도 함께 살펴보세요.",
+            text: "멍탐정의 산책길!\n수사 중 발견한 특별한 순간을\n단서로 저장해 보세요.",
             lottieName: "Onboarding2"),
         OnboardingStep(
             text: "수사가 끝나면 경로와 운동데이터,\n시간이 오늘의 수사일지에\n자동으로 기록돼요!",
